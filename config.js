e77f4615dca9f18b837a17c45f9b235008de5393
芋头 <xinyu198736@gmail.com>
2012-12-20 17:05:37 +0800

    udpate

--- config.js
+++ config.js
@@ -1,29 +1,28 @@
-
 module.exports = {
   "main_domain":"www.qwsns.com",
   "sdks" : {
     "sina" : {
-      "client_id" : "99395284",
-      "app_key" : "99395284",
-      "app_secret" : "abdee7a47ba5d96e646ffb2f1923b81d",
+      "client_id" : "test",
+      "app_key" : "test",
+      "app_secret" : "test",
       "redirect_uri" : "http://www.qwsns.com/sina_auth_cb"
     },
     "tqq" : {
-      "client_id" : "801287005",
-      "app_key" : "801287005",
-      "app_secret" : "4f84e329fb8a7638ee8c0bc0881a24b7",
+      "client_id" : "test",
+      "app_key" : "test",
+      "app_secret" : "test",
       "redirect_uri" : "http://www.qwsns.com/tqq_auth_cb"
     },
     "renren" : {
-      "client_id" : "99754adae54e49bc826da1144ad2659d",
-      "app_key" : "99754adae54e49bc826da1144ad2659d",
-      "app_secret" : "ddf05a79792a4a0aac6cfb42755e25c9",
+      "client_id" : "test",
+      "app_key" : "test",
+      "app_secret" : "test",
       "redirect_uri" : "http://www.qwsns.com/renren_auth_cb"
     },
     "douban" : {
-      "client_id" : "0bb4b9fc67f9b013231e2df537ed1039",
-      "app_key" : "0bb4b9fc67f9b013231e2df537ed1039",
-      "app_secret" : "e7434521cb0c70ad",
+      "client_id" : "test",
+      "app_key" : "test",
+      "app_secret" : "test",
       "redirect_uri" : "http://www.qwsns.com/douban_auth_cb"
     }
   }
